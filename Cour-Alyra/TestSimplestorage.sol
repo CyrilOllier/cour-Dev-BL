ac683593172bc075d7bcce6e58f49d1c3d2d9625
CyrilOllier <cyril_ollier@yahoo.frgit config --global user.email cyril_ollier@yahoo.frgit config --global user.email cyril_ollier@yahoo.fr>
2022-06-22 10:04:31 +0200

    envoi dossier et fichier cour Alyra

---
+++ Cour-Alyra/TestSimplestorage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.13;
+
+contract SimpleStorage {
+    uint  storageData;
+
+    event dataStored(uint _data,address _addr);
+
+
+
+    function get() public view returns(uint) {
+        return storageData;
+    }
+    
+    function set(uint n) public {
+        require(n>0, "vous ne pouvez pas mettre une valeur nulle");
+        storageData = n;
+        emit dataStored(n, msg.sender);
+    }
+
+}
